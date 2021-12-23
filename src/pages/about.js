72ad9db06e05337b81197441c94accd3620a43d3
John Connolly <35578555+jcconnol@users.noreply.github.com>
2021-12-23 13:44:14 -0600

    Adding google analytics (#23)
    * fixing star animation (#20)
    
    * fixing star animation
    
    * fixing star animation
    
    * adding small styling changes and GA head code

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,15 +20,28 @@ export default function Index() {
           <b>About</b>
         </h1>
         <p>
-          <b>I currently work at Ramsey Solutions as a test engineer and QA and previously worked as a software engineer.</b>
+          <b>I currently work in Nashville for Ramsey Solutions as a test engineer and previously worked as a software engineer in Indianapolis.</b>
         </p>
         <p>
-          I have been working in the technology sector for over three years and have loved every minute of it!
-          Learning new technologies and systems is a passion of mine so if you need help with a project of yours
+          I have been working in the technology sector for over four years and have loved every minute of it!
+          Learning new technologies and systems is a passion of mine. So, if you need help with a project,
           then I will be happy to help.
         </p>
         <p>
-          <b>Things I would be happy and able to help in:</b>
+          When attending University of Arkansas in Fayetteville, AR, I wanted to become a doctor.
+          I loved the problem solving and fast-paced environment of the medical field and thought it
+          was the only field that would allow me to work in that kind of environment. This led me to 
+          complete my Bachelor's of Science in Biology.
+        </p>
+        <p>
+          Half-way through my undergraduate degree, I found computer science and coding. I immediately knew it 
+          was the right path for me and I pursued a minor in computer science. Since then, I have never looked back 
+          and am very passionate about the software field and try to learn more about it every chance I get.
+        
+        </p>
+        <p>
+          <b>If you have a project that you would like help with these are a few 
+            things I would be happy join in with:</b>
         </p>
         <p>
           <ul>
@@ -36,19 +49,19 @@ export default function Index() {
               Formatting and outputting data in a RESTFUL API
             </li>
             <li>
+              Building of websites in React or Wordpress
+            </li>
+            <li>
+              Robotics for small-scale automated processes or funny use cases
+            </li>
+            <li>
               Test and QA webpage functionality and finding possible process improvements and application optimization areas
             </li>
             <li>
-              Test and QA application functionality as a 2nd or 3rd party.
+              Test and QA application functionality as a 2nd or 3rd party
             </li>
             <li>
               Automation of regression and confirmation tests
-            </li>
-            <li>
-              Building of website and its functionality
-            </li>
-            <li>
-              I also have experience in robotics for small-scale automated processes
             </li>
           </ul>
         </p>
