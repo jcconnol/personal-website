403fb4fc11fda5304eb86fcc43df81621c2bb185
John Connolly <jcconnol4@gmail.com>
2021-12-29 22:38:57 -0600

    fixing full page styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,20 +108,20 @@ module.exports = {
           policy: [{ userAgent: '*', allow: '/' }]
         }
       },
-      {
-         resolve: `gatsby-plugin-google-analytics`,
+       {
+         resolve: `gatsby-plugin-google-gtag`,
          options: {
-           // The property ID; the tracking code won't be generated without it
-           trackingId: "G-Z7KPCC51LW",
-           // Defines where to place the tracking script - `true` in the head and `false` in the body
-           head: true,
-           // Setting this parameter is optional
-           //anonymize: true,
-           // Setting this parameter is also optional
-           respectDNT: true,           
-           // Defers execution of google analytics script after page load
-           defer: false,
-           enableWebVitalsTracking: true,
+           // You can add multiple tracking ids and a pageview event will be fired for all of them.
+           trackingIds: [
+             "G-Z7KPCC51LW"
+           ],
+           // This object is used for configuration specific to this plugin
+           pluginConfig: {
+             // Puts tracking script in the head instead of the body
+             head: true,
+             // Setting this parameter is also optional
+             respectDNT: true,
+           },
          },
        },
        `gatsby-plugin-catch-links`,
