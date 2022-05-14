ee9a5708515b5cc0de34696965e19396ade6727b
John Connolly <jcconnol4@gmail.com>
2022-05-13 22:53:51 -0500

    add more projects

--- src/components/projectItems.js
+++ src/components/projectItems.js
@@ -6,6 +6,24 @@ const projectData = [
         githubLink: 'https://github.com/jcconnol/site-image-sizer-chrome-extension'
     },
     {
+        title: 'Prank Chrome Extension',
+        description: 'A chrome extension, written in vanilla JS, that has various pranks that can be applied to web pages such as making page images do the Harlem Shake or replace all pictures with Doge.',
+        projectLink: '',
+        githubLink: 'https://github.com/jcconnol/prank-chrome-extension'
+    },
+    {
+        title: 'Personal Jira Queue',
+        description: 'A simple Amplify hosted React App with a NodeJS API, this allows for you to make a list of tickets that you are working on so other coworkers can see what you are going to work on next.',
+        projectLink: '',
+        githubLink: 'https://github.com/jcconnol/jira-queue'
+    },
+    {
+        title: 'This website',
+        description: 'The personal website of me, John Connolly, that you are currently looking at and that is written in GatsbyJS.',
+        projectLink: '',
+        githubLink: 'https://github.com/jcconnol/personal-website'
+    },
+    {
       title: 'Game Automations',
       description: 'A set of python scripts that use win32gui to open a browser and auto play browser games.\n Two of the browser games are Doge Coin Miner and Sushi Roll.',
       projectLink: '',
