38f68964bb980e608276942ab5a58f90a8244167
John Connolly <jcconnol4@gmail.com>
2022-12-10 18:06:46 -0600

    add unity test page

---
+++ src/components/3d-object.js
@@ -0,0 +1,23 @@
+import * as React from "react"
+import "../styles/3d-object.css"
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+const ThreeDUnityObject = (props) => {
+    const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+        loaderUrl: "unity/build/build.loader.js",
+        dataUrl: "unity/build/webgl.data",
+        frameworkUrl: "unity/build/build.framework.js",
+        codeUrl: "unity/build/build.wasm",
+    });
+    
+    return (
+        <div className="game-container" >
+            <Unity
+                unityProvider={unityProvider}
+                style={{ width: "90%", height: "100%" }}
+            />
+        </div>
+    );
+}
+
+export default ThreeDUnityObject;
